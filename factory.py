df605054c6a1e832406d1a2170975dba3bd1d0cb
Roger <rogermb.en13@nycu.edu.tw>
2024-09-09 11:35:17 +0800

    Ya imprime el directorio

--- factory.py
+++ factory.py
@@ -7,6 +7,6 @@ class ChessPieceFactory:
         key=(name,color)
         if not cls._flyweights.get(key):
             cls._flyweights[key]= ChessPieceFlywight(name,color)
-        
+        print(cls._flyweights)
         return cls._flyweights[key]
 
